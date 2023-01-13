74db89203215d9f1aa79be5e0691493eb4de53cd
Shahwaiz <shahwaiz055@gmail.com>
2023-01-13 19:36:39 +0500

    formatting code using black PEP8

--- src/Rates.py
+++ src/Rates.py
@@ -4,29 +4,30 @@ import os
 
 logger = logging.getLogger(__name__)
 
+
 class Rates:
     """Encapsulates an Amazon DynamoDB table of rates data."""
+
     def __init__(self, dyn_resource):
         """
         :param dyn_resource: A Boto3 DynamoDB resource.
         """
         self.dyn_resource = dyn_resource
-        table_name = os.environ.get('DYNAMODB_TABLE', 'exchange-local')
+        table_name = os.environ.get("DYNAMODB_TABLE", "exchange-local")
         self.table = self.dyn_resource.Table(table_name)
-   
+
     def add_rates(self, rates):
         try:
             with self.table.batch_writer() as writer:
                 for rate in rates:
                     writer.put_item(Item=rate)
-                    print('Adding Rates:', rate)            
+                    print("Adding Rates:", rate)
         except ClientError as err:
             logger.error(
                 f"Couldn't load data into table {self.table.name}. Here's why: {err.response['Error']['Code']}: {err.response['Error']['Message']}"
             )
             raise
 
-    
     def get_rates(self, date):
         """
         Gets rates data from the table for a specific dates.
@@ -34,16 +35,14 @@ class Rates:
         :return: The data about the requested rates.
         """
         if type(date) is not str:
-            date  =str(date)
+            date = str(date)
         try:
-            response = self.table.get_item(Key={'time': date})
+            response = self.table.get_item(Key={"time": date})
         except ClientError as err:
             logger.error(
                 f"Couldn't get rates date= {date} from table {self.table.name}. Here's why: {err.response['Error']['Code']}: {err.response['Error']['Message']}"
             )
             raise
         else:
-            return response.get('Item', [])
+            return response.get("Item", [])
         pass
-
-
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -2,16 +2,18 @@ import requests
 import unittest
 from unittest import mock, TestCase
 from unittest.mock import MagicMock, patch
-#from src import helpers
+
+# from src import helpers
 from src import helpers
 from src import Rates
-import json 
+import json
 
-@mock.patch('helpers.requests.get')
+
+@mock.patch("helpers.requests.get")
 def test_fetch_rates(mock_requests):
     mock_response = MagicMock()
     mock_response.status_code = 200
-    with open('./tests/samples/eurofxref-hist-90d.xml') as f:
+    with open("./tests/samples/eurofxref-hist-90d.xml") as f:
         xml_file = f.read()
     mock_response.content = xml_file
     # specify the return value of the get() method
@@ -21,28 +23,26 @@ def test_fetch_rates(mock_requests):
     pass
 
 
-
-@mock.patch('helpers.dynamo_config')
-@mock.patch('Rates.Rates.__new__')
-@mock.patch('helpers.fetch_rates')
-def test_today_rates(mdynamo_config, mRates , mfetch_rates):
-    mdynamo_config = MagicMock(return_value=[])    
+@mock.patch("helpers.dynamo_config")
+@mock.patch("Rates.Rates.__new__")
+@mock.patch("helpers.fetch_rates")
+def test_today_rates(mdynamo_config, mRates, mfetch_rates):
+    mdynamo_config = MagicMock(return_value=[])
     mRates = MagicMock(return_value=[])
     mfetch_rates = MagicMock(return_value=[1, 2])
-    with patch('helpers.Rates.get_rates') as get_rates:
+    with patch("helpers.Rates.get_rates") as get_rates:
         get_rates = MagicMock(return_value=[])
         helpers.today_rates()
     pass
 
 
-
-@mock.patch('helpers.dynamo_config')
-@mock.patch('Rates.Rates.__new__')
-@mock.patch('helpers.today_rates')
+@mock.patch("helpers.dynamo_config")
+@mock.patch("Rates.Rates.__new__")
+@mock.patch("helpers.today_rates")
 def test_rate_comparison(mdynamo_config, mRates, mtoday_rates):
-    with open("./tests/samples/exchange_api_response.json") as exapi:    
+    with open("./tests/samples/exchange_api_response.json") as exapi:
         api_response = json.loads(exapi.read())
-    mdynamo_config = MagicMock(return_value=[])    
+    mdynamo_config = MagicMock(return_value=[])
     mRates = MagicMock()
     mtoday_rates = MagicMock(return_value=api_response[0])
     helpers.fetch_rates()
